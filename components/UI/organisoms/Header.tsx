c3e796d1d65edc2e1609e309b0207570282f0c49
vetan2 <vetan2@naver.com>
2022-11-03 22:32:50 +0900

    Add responsive style to head

--- components/UI/organisoms/Header.tsx
+++ components/UI/organisoms/Header.tsx
@@ -1,10 +1,13 @@
 import { MenuRounded } from "@mui/icons-material";
-import { IconButton } from "@mui/material";
+import { IconButton, useTheme } from "@mui/material";
 import Logo from "~/assets/images/logo.svg";
 import { Flex } from "~/components/UI/atoms/box";
+import { pxArray } from "~/lib/style";
 import { COLORS } from "~/styles/colors";
 
 export const Header = () => {
+  const theme = useTheme();
+
   return (
     <Flex
       component="header"
@@ -17,9 +20,22 @@ export const Header = () => {
         backgroundColor: "white",
         position: "sticky",
         inset: 0,
+
+        [theme.breakpoints.up("desktop")]: {
+          p: pxArray(16, 32),
+        },
       }}
     >
-      <IconButton sx={{ color: COLORS.MAIN, mr: 8 }}>
+      <IconButton
+        sx={{
+          color: COLORS.MAIN,
+          mr: 8,
+
+          [theme.breakpoints.up("desktop")]: {
+            display: "none",
+          },
+        }}
+      >
         <MenuRounded />
       </IconButton>
       <Logo css={{ height: 32 }} />
