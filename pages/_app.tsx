2f91b5e15abfe6ef6472c18f5d6910bca02d3b92
vetan2 <vetan2@naver.com>
2022-11-05 20:19:04 +0900

    Add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import {
   ThemeProvider as EmotionThemeProvider,
 } from "@emotion/react";
 import { ThemeProvider as MuiThemeProvider } from "@mui/material";
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import Layout from "~/pages/layout";
@@ -14,17 +15,20 @@ const cache = createCache({ key: "next" });
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <CacheProvider value={cache}>
-      <MuiThemeProvider theme={theme}>
-        <EmotionThemeProvider theme={theme}>
-          <RecoilRoot>
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          </RecoilRoot>
-        </EmotionThemeProvider>
-      </MuiThemeProvider>
-    </CacheProvider>
+    <>
+      <CacheProvider value={cache}>
+        <MuiThemeProvider theme={theme}>
+          <EmotionThemeProvider theme={theme}>
+            <RecoilRoot>
+              <Layout>
+                <Component {...pageProps} />
+              </Layout>
+            </RecoilRoot>
+          </EmotionThemeProvider>
+        </MuiThemeProvider>
+      </CacheProvider>
+      <Analytics />
+    </>
   );
 };
 
