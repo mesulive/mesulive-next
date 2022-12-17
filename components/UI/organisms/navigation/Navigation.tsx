b8106a8f0bef6e19e98f566b88cd2cc136870512
vetan2 <vetan2@naver.com>
2022-12-17 18:28:32 +0900

    Improve navigation animation more naturally

--- components/UI/organisms/navigation/Navigation.tsx
+++ components/UI/organisms/navigation/Navigation.tsx
@@ -1,5 +1,13 @@
-import { Box, Drawer, styled, Typography, useTheme } from "@mui/material";
-import { useRecoilState } from "recoil";
+import {
+  Box,
+  Drawer,
+  DrawerProps,
+  styled,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { forwardRef, useEffect } from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import Cube from "~/assets/images/cube.svg";
 import Flame from "~/assets/images/flame.svg";
 import Star from "~/assets/images/star.svg";
@@ -11,8 +19,9 @@ import {
 import { HoverBox } from "~/components/UI/organisms/navigation/HoverBox";
 import { NavLink } from "~/components/UI/organisms/navigation/NavLink";
 import { navigationStates } from "~/components/UI/organisms/navigation/store";
+import { useRefCallback } from "~/lib/hooks/ref";
 import { useScreenType } from "~/lib/hooks/window";
-import { pxArray } from "~/lib/style";
+import { pxArray, Sx } from "~/lib/style";
 import { COLORS } from "~/styles/colors";
 import { ScreenType } from "~/styles/constants";
 
@@ -21,59 +30,95 @@ const GroupTypography = styled(Typography)({
   fontWeight: "bold",
 });
 
+interface NavigationDrawerProps extends Sx {
+  variant: DrawerProps["variant"];
+  open?: DrawerProps["open"];
+}
+
+const NavigationDrawer = forwardRef<HTMLDivElement, NavigationDrawerProps>(
+  ({ sx: sxProp, variant, open }, ref) => {
+    const theme = useTheme();
+
+    const setOpen = useSetRecoilState(navigationStates.openAtom);
+
+    return (
+      <Drawer
+        ref={ref}
+        open={open}
+        variant={variant}
+        onClose={() => setOpen(false)}
+        PaperProps={{
+          sx: {
+            p: pxArray(32, 16),
+            zIndex: 20,
+            borderRightColor: COLORS.GRAY_6,
+            width: NAVIGATION_WIDTH,
+
+            [theme.breakpoints.up("tablet")]: {
+              height: `calc(100% - ${HEADER_HEIGHT}px)`,
+              top: HEADER_HEIGHT,
+            },
+          },
+        }}
+        transitionDuration={NAVIGATION_TOGGLE_DURATION}
+        sx={sxProp}
+      >
+        <Box component="nav">
+          <GroupTypography>기댓값 계산기</GroupTypography>
+          <NavLink href={"/calc/flame"} icon={Flame} sx={{ mt: 16 }}>
+            환생의 불꽃
+          </NavLink>
+          <NavLink
+            href={"/calc/cube"}
+            externalLink={"https://cubemesu.co"}
+            icon={Cube}
+            sx={{ mt: 16 }}
+          >
+            큐브
+          </NavLink>
+          <GroupTypography sx={{ mt: 32 }}>시뮬레이터</GroupTypography>
+          <NavLink
+            href={"/sim/starforce"}
+            externalLink={"https://mesu.live/sim/starforce"}
+            icon={Star}
+            sx={{ mt: 16 }}
+          >
+            스타포스
+          </NavLink>
+          <NavLink href={"/sim/cube"} disabled icon={Cube} sx={{ mt: 16 }}>
+            큐브
+          </NavLink>
+          <HoverBox />
+        </Box>
+      </Drawer>
+    );
+  }
+);
+
 export const Navigation = () => {
   const screenType = useScreenType();
-  const theme = useTheme();
+  const [firstDrawerRef, setFirstDrawerRef] = useRefCallback<HTMLDivElement>();
 
-  const [open, setOpen] = useRecoilState(navigationStates.openAtom);
+  const open = useRecoilValue(navigationStates.openAtom);
+
+  useEffect(() => {
+    setTimeout(() => {
+      firstDrawerRef.current?.remove();
+    }, NAVIGATION_TOGGLE_DURATION);
+  }, [firstDrawerRef]);
 
   return (
-    <Drawer
-      open={screenType >= ScreenType.desktop ? true : open}
-      variant={screenType < ScreenType.tablet ? "temporary" : "persistent"}
-      onClose={() => setOpen(false)}
-      PaperProps={{
-        sx: {
-          p: pxArray(32, 16),
-          zIndex: 20,
-          borderRightColor: COLORS.GRAY_6,
-          width: NAVIGATION_WIDTH,
-
-          [theme.breakpoints.up("tablet")]: {
-            height: `calc(100% - ${HEADER_HEIGHT}px)`,
-            top: HEADER_HEIGHT,
-          },
-        },
-      }}
-      transitionDuration={NAVIGATION_TOGGLE_DURATION}
-    >
-      <Box component="nav">
-        <GroupTypography>기댓값 계산기</GroupTypography>
-        <NavLink href={"/calc/flame"} icon={Flame} sx={{ mt: 16 }}>
-          환생의 불꽃
-        </NavLink>
-        <NavLink
-          href={"/calc/cube"}
-          externalLink={"https://cubemesu.co"}
-          icon={Cube}
-          sx={{ mt: 16 }}
-        >
-          큐브
-        </NavLink>
-        <GroupTypography sx={{ mt: 32 }}>시뮬레이터</GroupTypography>
-        <NavLink
-          href={"/sim/starforce"}
-          externalLink={"https://mesu.live/sim/starforce"}
-          icon={Star}
-          sx={{ mt: 16 }}
-        >
-          스타포스
-        </NavLink>
-        <NavLink href={"/sim/cube"} disabled icon={Cube} sx={{ mt: 16 }}>
-          큐브
-        </NavLink>
-        <HoverBox />
-      </Box>
-    </Drawer>
+    <>
+      <NavigationDrawer ref={setFirstDrawerRef} open variant={"permanent"} />
+      {screenType >= ScreenType.tablet && (
+        <NavigationDrawer
+          variant={"persistent"}
+          open={screenType >= ScreenType.desktop || open}
+        />
+      )}
+      {screenType < ScreenType.tablet && (
+        <NavigationDrawer variant={"temporary"} open={open} />
+      )}
+    </>
   );
 };
